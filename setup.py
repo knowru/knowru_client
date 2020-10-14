b91a5ea537d452c874d341df3cd63dd62b1adc85
Jihae Hwang <jihaekor@gmail.com>
2020-10-15 07:08:40 +0900

    adding timeout parameter; up version to 0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='knowru_client',
     packages=['knowru_client'],
-    version='0.1',
+    version='0.2',
     description='Client to run runnables in Knowru',
     author='Knowru, LLC',
     author_email='hello@knowru.com',
     license='MIT',
     url='https://github.com/knowru/knowru_client',
-    download_url='https://github.com/knowru/knowru_client/archive/0.1.zip',
+    download_url='https://github.com/knowru/knowru_client/archive/0.2.zip',
     keywords='knowru client',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
